1bc9870d81451854ae87c45692a6a3bcb7253d00
황동민 <hdmin8712@gmail.com>
2025-05-11 23:33:35 +0900

    패키지파일 수정

--- Package.swift
+++ Package.swift
@@ -1,34 +1,22 @@
 // swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
-    name: "MyLibrary",
-    platforms: [
-        .iOS(.v13)
-    ],
+    name: "AppUtilities",
+    platforms: [.iOS(.v13)],
     products: [
         .library(
-            name: "MyLibrary",
-            targets: ["MyLibrary"]
-        )
-    ],
-    dependencies: [
-        // 외부 의존성 필요 시 여기에 추가
+            name: "AppUtilities",
+            targets: ["AppUtilities"]),
     ],
     targets: [
         .target(
-            name: "MyLibrary",
-            dependencies: [],
-            path: "Sources"
-        ),
+            name: "AppUtilities",
+            dependencies: []),
         .testTarget(
-            name: "MyLibraryTests",
-            dependencies: ["MyLibrary"],
-            path: "Tests"
-        )
+            name: "AppUtilitiesTests",
+            dependencies: ["AppUtilities"]),
     ]
 )
-
